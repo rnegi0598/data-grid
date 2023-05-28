da0d7629167790ce90e034082d5c6abba12739cb
rnegi0598 <rnegi0598@gmail.com>
2023-05-28 23:13:43 +0530

    jsonplaceholder

--- src/services/jsonplaceholder.js
+++ src/services/jsonplaceholder.js
@@ -1,53 +1,66 @@
 // Need to use the React-specific entry point to import createApi
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // Define a service using a base URL and expected endpoints
 export const placeholderApi = createApi({
-  reducerPath: 'placeholderApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://jsonplaceholder.typicode.com' }),
+  reducerPath: "placeholderApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://jsonplaceholder.typicode.com",
+  }),
   keepUnusedDataFor: 60,
   endpoints: (builder) => ({
     getUser: builder.query({
-      query: ({start,end}) => {
-        if(start===-1 && end==-1){
-          return "users"
+      query: ({ start, end }) => {
+        if (start === -1 && end === -1) {
+          return "users";
         }
-        return `users?_start=${start}&_end=${end}`
+        return `users?_start=${start}&_end=${end}`;
       },
-      transformResponse: (response) => {
+      transformResponse: (response, meta, arg) => {
         // Modify the response data here
-        return response.map((item) => ({
+        const { category, value } = arg;
+        const updatedArr = response.map((item) => ({
           id: item.id,
           name: item.name,
-          username:item.username,
-          email:item.email,
-          address:item.address.street+" "+item.address.city,
-          phone:item.phone,
-          website:item.website,
+          username: item.username,
+          email: item.email,
+          address: item.address.street + " " + item.address.city,
+          phone: item.phone,
+          website: item.website,
         }));
+
+        if (category === "all" && value !== "") {
+          return updatedArr.filter((item) => {
+            const valuesArray = Object.values(item);
+            if (valuesArray.includes(value)) {
+              return true;
+            }
+            return false;
+          });
+        }
+        return updatedArr;
       },
     }),
     getPosts: builder.query({
-      query: ({start,end}) => {
-        
-        if(start===-1 && end===-1){
-          return "posts"
+      query: ({ start, end }) => {
+        if (start === -1 && end === -1) {
+          return "posts";
         }
-        return `posts?_start=${start}&_end=${end}`
-      }
-        ,
-      }),
-      getComments: builder.query({
-        query: ({start,end}) => {
-          if(start===-1 && end==-1){
-            return "comments"
-          }
-          return `comments?_start=${start}&_end=${end}`
-        },
-      }),
+        return `posts?_start=${start}&_end=${end}`;
+      },
+    }),
+    getComments: builder.query({
+      query: ({ start, end }) => {
+        if (start === -1 && end === -1) {
+          return "comments";
+        }
+        return `comments?_start=${start}&_end=${end}`;
+      },
+    }),
   }),
-})
+});
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
-export const { useGetUserQuery, useGetPostsQuery,useGetCommentsQuery } = placeholderApi;
+export const { useGetUserQuery, useGetPostsQuery, useGetCommentsQuery } =
+  placeholderApi;
