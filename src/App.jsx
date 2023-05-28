6edbdbe563b9607df626757c03afd534f95d69e4
rnegi0598 <rnegi0598@gmail.com>
2023-05-28 19:33:56 +0530

    setCurrentPage to 1 on table change

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,18 @@ import DataGrid from "./components/dataGrid/DataGrid";
 
 function App() {
   const [activeType, setActiveType] = useState("users");
+  const [currentPage,setCurrentPage]=useState(1);
 
   const activeHandler = (type) => {
     if (type !== activeType) setActiveType(type);
+    setCurrentPage(1);
   };
 
   return (
     <div className="app-wrapper">
       <div className="app">
         <Navbar activeType={activeType} activeHandler={activeHandler} />
-        <DataGrid activeType={activeType} />
+        <DataGrid activeType={activeType} currentPage={currentPage} setCurrentPage={setCurrentPage} />
       </div>
     </div>
   );
--- src/components/dataGrid/DataGrid.jsx
+++ src/components/dataGrid/DataGrid.jsx
@@ -3,18 +3,32 @@ import "./datagrid.scss";
 import Users from "../../pages/users/Users";
 import Posts from "../../pages/posts/Posts";
 import Comments from "../../pages/comments/Comments";
-import {useState} from 'react'
-const DataGrid = ({ activeType }) => {
-  const [currentPage,setCurrentPage]=useState(1);
-  const currentPageHandler=(pageNumber)=>{
+
+const DataGrid = ({ activeType, currentPage, setCurrentPage }) => {
+  const currentPageHandler = (pageNumber) => {
     setCurrentPage(pageNumber);
-  }
+  };
   if (activeType === "users") {
-    return <Users currentPage={currentPage} currentPageHandler={currentPageHandler} />;
+    return (
+      <Users
+        currentPage={currentPage}
+        currentPageHandler={currentPageHandler}
+      />
+    );
   } else if (activeType === "posts") {
-    return <Posts currentPage={currentPage} currentPageHandler={currentPageHandler} />;
+    return (
+      <Posts
+        currentPage={currentPage}
+        currentPageHandler={currentPageHandler}
+      />
+    );
   } else {
-    return <Comments currentPage={currentPage} currentPageHandler={currentPageHandler} />;
+    return (
+      <Comments
+        currentPage={currentPage}
+        currentPageHandler={currentPageHandler}
+      />
+    );
   }
 };
 
